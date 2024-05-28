233c9c4ec3f2674f553109e1eccde25eb9882408
stikkie13 <stikkie13@gmail.com>
2024-05-28 18:41:41 +0200

    main bestand voor arduino mega gemaakt

---
+++ src/main.ino
@@ -0,0 +1,128 @@
+#include <Arduino.h>
+#include <Keypad.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN 53  // MEGA: 53
+#define RST_PIN 48  // MEGA: 5
+
+#define button1 6
+#define button2 5
+#define button3 4
+#define button4 3
+#define button5 2
+#define button6 14
+
+int buttons[6] = {
+        button1,
+        button2,
+        button3,
+        button4,
+        button5,
+        button6
+};
+
+String IBAN = "";
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+
+const byte rows = 4;
+const byte cols = 4;
+char keys[rows][cols] = {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'},
+        {':', '0', ';'}
+};
+byte rowPins[rows] = {1, 2, 3, 4};
+byte colPins[cols] = {5, 6, 7};
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+
+
+void checkCard(){
+  byte bufferATQA[2];
+  byte bufferSize = sizeof(bufferATQA);
+  MFRC522::StatusCode result = mfrc522.PICC_WakeupA(bufferATQA, &bufferSize);
+  
+  if (result == MFRC522::STATUS_OK || result == MFRC522::STATUS_COLLISION) {
+    // Select one of the cards
+    if (mfrc522.PICC_ReadCardSerial()) {
+      MFRC522::MIFARE_Key key;
+      for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+      IBAN = "";
+      readBlock(4, key, IBAN);
+      readBlock(5, key, IBAN);
+
+      mfrc522.PICC_HaltA();
+      mfrc522.PCD_StopCrypto1();
+    }
+  }else{
+    IBAN = "";
+  }
+}
+
+bool readBlock(byte block, MFRC522::MIFARE_Key &key, String &data) {
+  byte buffer[18];
+  byte len = 18;
+  MFRC522::StatusCode status;
+
+  // Authenticate with the block
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed for block "));
+    Serial.print(block);
+    Serial.print(F(": "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    // return false;
+  }
+
+  // Read data from the block
+  status = mfrc522.MIFARE_Read(block, buffer, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed for block "));
+    Serial.print(block);
+    Serial.print(F(": "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    // return false;
+  }
+
+  // Process and append data to IBAN string
+  for (uint8_t i = 0; i < 16; i++) {
+    if (buffer[i] != 0) {
+      IBAN += (char)buffer[i];
+    } else {
+      break;
+    }
+  }
+
+  return true;
+}
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial communications with the PC
+  SPI.begin();         // SPI bus
+  mfrc522.PCD_Init();  // Initialise MFRC522
+  for (int b: buttons) {
+    pinMode(b, INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  // checkCard();
+  // Serial.print(F("IBAN: "));
+  // Serial.println(IBAN);
+  char key = keypad.getKey();
+  if (key){
+    Serial.write(key-'0'+200);
+  }
+
+  int button = 0;
+  for (int b:buttons) {
+    if (!digitalRead(b)){
+      button = b;
+    }
+  }
+  if (button) {
+    Serial.write(button-7+100);
+  }
+  delay(100);
+}
